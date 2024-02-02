ff6d354518a952e71f67674dfef4d5f9866f9aed
matteogu <matteo.guarrera14@gmail.com>
2024-02-02 14:55:31 -0800

    plots

--- homework1/problem2_burgers/train.py
+++ homework1/problem2_burgers/train.py
@@ -22,7 +22,7 @@ def train():
     # Hyperparameters
     lr = 5e-3
     batch_size = 16
-    epochs = 40
+    epochs = 2 #40
 
     # Setup optimizer, model, data loader etc.
     # TODO
@@ -46,9 +46,11 @@ def train():
     optimizer = optim.Adam(model.parameters(), lr=lr)
     criterion = torch.nn.MSELoss()
 
+    alpha = 1.0
+    loss_name = 'residual_loss'  #'data_loss' # 'residual_loss' , f'combined_loss_alpha'
+
     # Training Loop
     # TODO
-    num_epochs = 40
     train_losses = []
     for epoch in tqdm(range(epochs)):
         running_loss = 0.0
@@ -56,16 +58,19 @@ def train():
             optimizer.zero_grad()
             outputs = model(inputs)
 
-            # loss = burgers_data_loss(outputs, labels)
-
-            residual = burgers_pde_residual_fast(dx, dt, outputs)
-            loss = criterion(residual, torch.zeros_like(residual))
+            if loss_name == 'data_loss':
+                loss = burgers_data_loss(outputs, labels)
+            elif loss_name == 'residual_loss':
+                residual = burgers_pde_residual_fast(dx, dt, outputs)
+                loss = criterion(residual, torch.zeros_like(residual))
+            elif loss_name == 'combined_loss_alpha':
+                pass
 
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
             train_losses.append(loss.item())
-        print(f'Epoch: {epoch}  {running_loss}, {np.mean(train_losses[-50:])}')
+        print(f'Epoch: {epoch} {running_loss} {loss.item()}, {np.mean(train_losses[-5:])}')
 
     # ONLY DATA LOSS  Epoch: 39  1300.1681728363037, 20.431795234680177
     # Evaluate dataset on pde loss: Mean: 0.10332986782849865 	 Std: 0.19170095194512526
@@ -73,25 +78,57 @@ def train():
 
 
     # Visualize the training loss
-    plt.plot(range(num_epochs), train_losses, label='Training Loss')
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.legend()
-    plt.show()
+    fig, (ax1, ax2) = plt.subplots(1,2, figsize=(5, 15))
+    ax1.plot(train_losses, label='Training Loss')
+    ax1.grid()
+    ax1.set_xlabel('Epochs')
+    ax1.set_ylabel('Loss')
+    ax1.legend()
 
+    ax2.plot(train_losses[-10:], label='Training Loss')
+    ax2.grid()
+    ax2.set_xlabel('Epochs')
+    ax2.set_ylabel('Loss')
+    ax2.legend()
+
+    plt.savefig(f'training_{loss_name}.pdf')
+    plt.show()
     # Evaluate the model on the test dataset and visualize predictions for the first 3 instances
     model.eval()
 
+    sample_test, target_test = next(iter(test_loader))
+    outputs_test = model(sample_test)
+
+    fig, ax = plt.subplots(3, 2, figsize=(5, 3))
+    for i in range(3):
+
+        ax[i, 0].imshow(outputs_test[i].detach().cpu().numpy())
+        ax[i, 0].set_title(f'[sample # {i}] Prediction')
+
+        ax[i, 1].imshow(target_test[i].detach().cpu().numpy())
+        ax[i, 1].set_title(f'[sample # {i}] Ground Truth')
+
+    plt.savefig('test_set_images.pdf')
+    plt.show()
+
     # Validation Loop
     # TODO
-    fig, (ax1, ax2, ax3) = plt.subplots(3,1, figsize=(10,5))
+
+
     with torch.no_grad():
+        test_losses = []
         for i, (inputs, labels) in enumerate(test_loader):
             if i >= 3:
                 break
             outputs = model(inputs)
             print(outputs.size())
 
+
+            residual = burgers_pde_residual_fast(dx, dt, outputs)
+            loss = criterion(residual, torch.zeros_like(residual))
+
+            test_losses.append(loss.item())
+    plt.plot(test_losses, label='Test Loss')
 
 if __name__ == '__main__':
     torch.manual_seed(0)
