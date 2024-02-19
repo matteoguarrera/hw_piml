06cafaef1a70b8437c7709041705455202eb77ab
matteogu <matteo.guarrera14@gmail.com>
2024-02-18 20:21:56 -0800

    hw2_started

--- homework2/problem4-schnet/schnet.py
+++ homework2/problem4-schnet/schnet.py
@@ -11,6 +11,12 @@ from torch.nn import Embedding, Linear, ModuleList, Sequential
 from torch_scatter import scatter
 from torch_geometric.nn import radius_graph
 
+
+'''
+pip install ase, lmdb, wandb
+pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-2.0.1+cu117.html
+
+'''
 class SchNet(torch.nn.Module):
     r"""The continuous-filter convolutional neural network SchNet from the
     `"SchNet: A Continuous-filter Convolutional Neural Network for Modeling
@@ -76,7 +82,7 @@ class SchNet(torch.nn.Module):
         self.std = std
         self.scale = None
 
-        atomic_mass = torch.from_numpy(ase.data.atomic_masses)
+        self.atomic_mass = torch.from_numpy(ase.data.atomic_masses)
 
         self.embedding = Embedding(100, hidden_channels)
         self.distance_expansion = GaussianSmearing(0.0, cutoff, num_gaussians)
@@ -108,23 +114,55 @@ class SchNet(torch.nn.Module):
         batch = torch.zeros_like(z) if batch is None else batch
         pos.requires_grad = True
 
-        h = #TODO: compute initial node representations
+        print(z.shape, pos.shape)
+
+        # TO DO: compute initial node representations
+        h = self.embedding(z)  # Correct
+
+        # Computes graph edges to all points within a given distance.
+        # DOC: https://pytorch-geometric.readthedocs.io/en/1.3.1/modules/nn.html#torch_geometric.nn.pool.radius_graph
         edge_index = radius_graph(pos, r=self.cutoff, batch=batch, max_num_neighbors=self.max_num_neighbors)
-        
-        edge_weight = #TODO: compute pairwise distances
-        edge_attr = #TODO: compute radial basis expansion of pairwise distances
+        # edge_index <= return torch.stack([row, col], dim=0)   [torch.Long]
+
+        # TO DO: compute pairwise distances
+        row, col = edge_index
+        edge_weight = (pos[row] - pos[col]).norm(dim=-1)
+
+        # TO DO: compute radial basis expansion of pairwise distances
+        edge_attr = self.distance_expansion(edge_weight)  # Correct
 
         for interaction in self.interactions:
-            #TODO: call interaction layers
+            # TODO: call interaction layers
+            # InteractionBlock, Figure 2 from the paper
+            h = h + interaction(h, edge_index, edge_weight, edge_attr)
+
 
         #linear projection to obtain atomwise energies
         h = self.lin1(h)
         h = self.act(h)
         h = self.lin2(h)
 
-        energy = #TODO: compute global energies per molecule
+        # TODO: compute global energies per molecule
+        """ Why all of this? 
+        Just because it's there in the original codebase? """
+        if self.dipole:
+            mass = self.atomic_mass[z].view(-1, 1)
+            c = scatter(mass * pos, batch, dim=0) / scatter(mass, batch, dim=0)
+            h = h * (pos - c[batch])
 
-        forces = #TODO: compute forces
+        if not self.dipole and self.mean is not None and self.std is not None:
+            # scale energy contributions in the original codebase
+            h = h * self.std + self.mean
+
+        energy = scatter(h, batch, dim=0, reduce=self.readout)
+
+        # TODO: compute forces
+        forces = -1 * torch.autograd.grad(energy,
+                                          pos,
+                                          grad_outputs=torch.ones_like(energy),
+                                          create_graph=True,
+                                          retain_graph=True)[0]
+        # forces = -energy.grad
 
         return energy, forces
 
@@ -154,7 +192,8 @@ class InteractionBlock(torch.nn.Module):
         self.lin.bias.data.fill_(0)
 
     def forward(self, h, edge_index, edge_weight, edge_attr):
-        h = #TODO: fill in this line
+        # TO DO: fill in this line
+        h = self.conv(h, edge_index, edge_weight, edge_attr)   # Correct
         h = self.act(h)
         h = self.lin(h)
         return h
@@ -175,10 +214,27 @@ class CFConv(torch.nn.Module):
         self.lin2.bias.data.fill_(0)
 
     def forward(self, h, edge_index, edge_weight, edge_attr):
+        """ From the paper:
+        1) RBF,                  2) Dense,
+        3) Shifted Softplus,     4) Dense,
+        5) Shifted Softplus,     6) Out """
+
+        print('h ', h.shape,'edge idx ', edge_index.shape,
+              'edge wei ',  edge_weight.shape, 'edge attr ', edge_attr.shape)
+
         h = self.lin1(h)
-        c = #TODO: transform edge weights to [0,1]
-        W = #TODO: compute filters
-        h = #TODO: perform graph convolution
+
+        # TO DO: transform edge weights to [0,1], aka use the equation with cosine
+        c = 0.5 * (torch.cos(edge_weight *torch.pi/self.cutoff) +1.0)  # Correct
+        print('c ', c.shape)
+
+        # TO DO: compute filters
+        W = self.nn(edge_attr)* c * h  # W_theta in the text of the HW
+        print('W ', W.shape)
+
+        # TODO: perform graph convolution
+        # DOC: https://pytorch-scatter.readthedocs.io/en/latest/functions/scatter.html
+        h = scatter(W, edge_index, dim=1, reduce="sum")
         h = self.lin2(h)
         return h
         
@@ -187,12 +243,16 @@ class CFConv(torch.nn.Module):
 class GaussianSmearing(torch.nn.Module):
     def __init__(self, start=0.0, stop=5.0, num_gaussians=50):
         super().__init__()
-        offset = torch.linspace(start, stop, num_gaussians)
+        offset = torch.linspace(start, stop, num_gaussians)  # positions of the centers
         self.gamma = 0.5 / (offset[1] - offset[0]).item()**2
         self.register_buffer('offset', offset) #sets the attribute self.offset = offset
 
     def forward(self, dist):
         #TODO: implement this function
+        out = torch.exp(-self.gamma*(dist-self.offset)**2)
+        # assert
+        print(dist.shape, self.offset.shape, self.gamma.shape, out.shape)
+        return out
 
 class ShiftedSoftplus(torch.nn.Module):
     def __init__(self):
--- homework2/problem4-schnet/train.py
+++ homework2/problem4-schnet/train.py
@@ -7,23 +7,25 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 from utils import L2MAELoss, rotate_3d_coordinates
 import wandb
+
+
 def train(data_dir, size, r_max, batch_size, lr, max_epochs, device):
 
-    #W&B Run
-    wandb.login()
-    run = wandb.init(
-    # Set the project where this run will be logged
-    project="dl-physics-hw2-schnet",
-    name=f"aspirin_{size}_r_max={r_max}",
-    # Track hyperparameters and run metadata
-    config={
-        "learning_rate": lr,
-        "epochs": max_epochs,
-        "batch_size": batch_size,
-        "r_max": r_max,
-        "size": size
-    }
-    )
+    # # W&B Run
+    # wandb.login()
+    # run = wandb.init(
+    #     # Set the project where this run will be logged
+    #     project="dl-physics-hw2-schnet",
+    #     name=f"aspirin_{size}_r_max={r_max}",
+    #     # Track hyperparameters and run metadata
+    #     config={
+    #         "learning_rate": lr,
+    #         "epochs": max_epochs,
+    #         "batch_size": batch_size,
+    #         "r_max": r_max,
+    #         "size": size
+    #     }
+    # )
 
     # data
     train_dir = f"{data_dir}/{size}/train"
@@ -33,18 +35,18 @@ def train(data_dir, size, r_max, batch_size, lr, max_epochs, device):
     val_dataset = LmdbDataset({'src': val_dir})
     test_dataset = LmdbDataset({'src': test_dir})
     train_dataloader = DataLoader(train_dataset, collate_fn=data_list_collater, \
-                                batch_size = batch_size, shuffle = False)
+                                  batch_size=batch_size, shuffle=False)
     val_dataloader = DataLoader(val_dataset, collate_fn=data_list_collater, \
-                                batch_size = batch_size, shuffle = False)
+                                batch_size=batch_size, shuffle=False)
     test_dataloader = DataLoader(test_dataset, collate_fn=data_list_collater, \
-                                batch_size = batch_size, shuffle = False)
+                                 batch_size=batch_size, shuffle=False)
     # model
-    model = SchNet(cutoff = r_max)
+    model = SchNet(cutoff=r_max)
     model = model.to(device)
 
-    #optimization
+    # optimization
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.2, patience=10)    
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.2, patience=10)
     force_loss = L2MAELoss()
 
     epoch = 0
@@ -52,24 +54,35 @@ def train(data_dir, size, r_max, batch_size, lr, max_epochs, device):
         model.train()
         print(f"Train Epoch {epoch + 1}")
         for batch in tqdm(train_dataloader):
-            #TODO: fill in the training loop
-            wandb.log({"train_force_loss": loss, "lr": optimizer.param_groups[0]['lr']})
-        
+            # TODO: fill in the training loop
+            print(batch)
+            optimizer.zero_grad()
+            out_energy, out_forces = model.forward(batch.atomic_numbers, batch.pos, batch=None)
+            for name, value in batch:
+                if value.dtype == torch.long:
+                    print(name, torch.max(value))
+            # loss.backward()
+            # optimizer.step()
+            #
+            # print({"train_force_loss": loss, "lr": optimizer.param_groups[0]['lr']})
+            # wandb.log({"train_force_loss": loss, "lr": optimizer.param_groups[0]['lr']})
+
         model.eval()
         print(f"Val Epoch {epoch + 1}")
         for batch in val_dataloader:
-            #TODO: fill in validation loop
+            # TODO: fill in validation loop
             wandb.log({"val_force_loss": loss})
         scheduler.step(mean_val_loss)
-        
-        epoch +=1
-    
-    #Final Test
+
+        epoch += 1
+
+    # Final Test
     test_losses = []
     for batch in test_dataloader:
-        #TODO: fill in test loop
+        # TODO: fill in test loop
         test_losses.append(loss.mean().item())
     wandb.log({"test_force_loss": sum(test_losses) / len(test_losses)})
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -77,10 +90,9 @@ if __name__ == "__main__":
     parser.add_argument('--size', type=str, default='1k', help='Size of dataset')
     parser.add_argument('--r_max', type=float, default=5.0, help='Cutoff (A) for radius graph construction')
     parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')
-    parser.add_argument('--data_dir', type=str, default="/data/sanjeevr/md17/aspirin/", help='Directory of data')
+    parser.add_argument('--data_dir', type=str, default="aspirin", help='Directory of data')
     parser.add_argument('--batch_size', type=int, default=128, help='Batch size')
     parser.add_argument('--max_epochs', type=int, default=1000, help='Number of epochs')
-
 
     args = parser.parse_args()
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
